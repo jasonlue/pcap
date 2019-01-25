0620edb41f9b86ae0b7f4aff374099fc5646931d
jasonlue <jasonlue@hotmail.com>
2019-01-25 00:59:51 -0800

    skip if folder exists

--- bro-gen.sh
+++ bro-gen.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #this will take forever to finish. esp. 10M 100M as input. massif may take more than 24 hours. run wih caution.
-input=(1K 10K)
-# 100K 1M 10M 100M)
-cmd=(prof massif)
+input=(1K 10K 100K 1M 10M 100M)
+cmd=(run prof massif)
 
 for i in ${input[@]}; do
     for c in ${cmd[@]}; do
-        bro.sh $i $c
+        if [ ! -d bro-$i-$c ]; then
+            bro.sh $i $c
+        fi
     done
 done
